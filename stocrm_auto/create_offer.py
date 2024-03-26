8b8ab93323b659dbef159e6427cc69c7d2e16fdd
Paul Versailles <yeah.inside@inbox.ru>
2024-03-26 17:22:09 +0300

    fix

--- stocrm_auto/create_offer.py
+++ stocrm_auto/create_offer.py
@@ -4,16 +4,18 @@ from selenium.webdriver.common.by import By
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
-driver = webdriver.Chrome()
+options.add_argument("--disable-notifications")
+driver = webdriver.Chrome(options)
 driver.set_window_size(1920,1080)
 driver.get("http://demo.stocrm.ru")
-login_form = driver.find_element(By.CSS_SELECTOR, "[type = Text]").send_keys("")
-password_form = driver.find_element(By.CSS_SELECTOR, "[type = Password]").send_keys("")
+login_form = driver.find_element(By.CSS_SELECTOR, "[type = Text]").send_keys("p")
+password_form = driver.find_element(By.CSS_SELECTOR, "[type = Password]").send_keys("CLG")
 button = driver.find_element(By.CSS_SELECTOR, "div.square_button_wrapper").click()
-#offer_create = driver.find_element(By.CSS_SELECTOR, "square_button_wrapper.middle_size_dark_blue")
-offer_create = driver.find_element(By.CLASS_NAME, "add_card_column_button")
-time.sleep(5)
+driver.implicitly_wait(10)
+offer_create_1 = driver.find_element(By.ID, 'add_card_column_button_hint').click()
+time.sleep(5) 
 if offer_create is None:
    print("Элемент не найден")
 else:
    print("Элемент найден")
+   
